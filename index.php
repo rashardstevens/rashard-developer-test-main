2d2b50745e9340a6d83414232924a756b73dc6bd
Rashard Stevens <rashardstevenswork@gmail.com>
2022-08-16 23:58:04 -0400

    update

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>KHM Travel Developer Test</title>
+</head>
+
+<body>
+    <div class="hero">
+        <div class="input-group">
+    <form action="insert-data.php" method="post">
+        <p>Best televison show of all time:</p>
+        <input type="text" name="tv-show">
+
+        <input type="submit" value="Save to database" class="button">
+    </form>
+        </div>
+    </div>
+</body>
+
+</html>
